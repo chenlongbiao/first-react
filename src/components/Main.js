fcda342e302f2bc54afdbe89fc6381531233f7e4
陈龙飚 <714147655@qq.com>
2017-06-15 21:28:00 +0800

    1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,40 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+//引入图片资源
+var  imageDatas=require('../data/imageDatas.json');
 
-class AppComponent extends React.Component {
+// let yeomanImage = require('../images/yeoman.png');
+//将图片资源json对象中的图片路径拿到并写入图片资源信息对象中
+function getImageDataUrl(imageDatasArr) {
+  for (let i=0;i<imageDatasArr.length;i++){
+     let imageData = imageDatasArr[i];
+     imageData.imageURL = require('../images/'+imageData.fileName);
+      imageDatasArr[i]=imageData
+  }
+  return imageDatasArr
+}
+
+imageDatas=getImageDataUrl(imageDatas);
+class firstReact extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      //舞台
+      <section className="stage">
+        <section className="img-sec">
+
+        </section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
+firstReact.defaultProps = {
 };
 
-export default AppComponent;
+export default firstReact;
